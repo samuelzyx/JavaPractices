51ca24b5ebc5377fb53c287524df9264b541f89d
Sam Seda <zyxsamys@gmail.com>
2024-03-08 12:33:48 -0600

    User deleteById

--- UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/user/UserResource.java
+++ UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/user/UserResource.java
@@ -7,6 +7,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,5 +48,10 @@ public class UserResource {
 
         //Response location redirect uri in Headers
         return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        service.deleteById(id);
     }
 }
