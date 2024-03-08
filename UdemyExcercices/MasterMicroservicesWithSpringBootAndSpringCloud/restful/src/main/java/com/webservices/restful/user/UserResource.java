7648e631f064fdb3b78e7d2d0212d43a2d8da01e
Sam Seda <zyxsamys@gmail.com>
2024-03-07 19:12:38 -0600

    Refactor Directory

---
+++ UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/user/UserResource.java
@@ -0,0 +1,36 @@
+package com.webservices.restful.user;
+
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service){
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+
+    @PostMapping("/users")
+    public void createUser(@RequestBody User user) {
+        service.save(user);
+    }
+    
+}
