56592e776fceb9f95c8dd3d094ca099f2723c1ae
Sam Seda <zyxsamys@gmail.com>
2024-03-07 18:39:13 -0600

    HelloWorld Path Variable

--- UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/restful/src/main/java/com/webservices/restful/helloworld/HelloWorldController.java
+++ UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/restful/src/main/java/com/webservices/restful/helloworld/HelloWorldController.java
@@ -2,7 +2,7 @@ package com.webservices.restful.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorldController {
@@ -11,4 +11,16 @@ public class HelloWorldController {
     public String helloWorld(){
         return "Hello World";
     }
+
+    @GetMapping("/hello-world/bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+    
+    
 }
