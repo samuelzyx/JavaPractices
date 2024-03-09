bdbaad176253d0d5717054d679e5af5f19241bc4
Sam Seda <zyxsamys@gmail.com>
2024-03-08 19:18:21 -0600

    Versioning Person

--- UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/versioning/VersioningPersonController.java
+++ UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/versioning/VersioningPersonController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
+
 @RestController
 public class VersioningPersonController {
     @GetMapping("/v1/person")
@@ -15,4 +16,25 @@ public class VersioningPersonController {
     public PersonV2 getSecondVersionPerson() {
         return new PersonV2(new Name("Bob", "Charlie"));
     }    
+
+    @GetMapping(path="/person",params="version=1")
+    public PersonV1 getFirstVersionPersonRequestParameter() {
+        return new PersonV1("Bob Charlie");
+    }
+    
+    @GetMapping(path="/person",params="version=2")
+    public PersonV2 getSecondVersionPersonRequestParameter() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }    
+
+    @GetMapping(path="/person/header",headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionPersonHeader() {
+        return new PersonV1("Bob Charlie");
+    }
+
+    @GetMapping(path="/person/header",headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionPersonHeader() {
+        return new PersonV2(new Name("Bob", "Charlie"));
+    }
+    
 }
