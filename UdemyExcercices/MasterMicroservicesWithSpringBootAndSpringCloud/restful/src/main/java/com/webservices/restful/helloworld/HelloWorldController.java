d75e4b1d45d85369ca327c3b3945a76f0f9cf829
Sam Seda <zyxsamys@gmail.com>
2024-03-08 18:37:05 -0600

    Adding Internationalization responses

--- UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/helloworld/HelloWorldController.java
+++ UdemyExcercices/MasterMicroservicesWithSpringBootAndSpringCloud/restful/src/main/java/com/webservices/restful/helloworld/HelloWorldController.java
@@ -1,15 +1,34 @@
 package com.webservices.restful.helloworld;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class HelloWorldController {
 
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/good-morning-internationalized")
+    public String goodMorningInternationalized(){
+
+        Locale locale = LocaleContextHolder.getLocale();
+        
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 
     @GetMapping("/hello-world/bean")
